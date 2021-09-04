028a772261c56b2ea213b5660ed2bdc1a15f9e09
Martins Pavlovskis <martinspavlovskis@yahoo.com>
2021-09-04 22:41:45 +0300

    tests done

--- sql-movies/test/03-insert-combined-data.test.ts
+++ sql-movies/test/03-insert-combined-data.test.ts
@@ -11,7 +11,9 @@ import { Rating } from "../src/data/types";
 import { minutes } from "./utils";
 
 const insertRatings = (movieId: number, ratings: Rating[]) => {
-  throw new Error(`todo`);
+  return (
+    `INSERT INTO movie_ratings (movie_id, user_id, rating, time_created) VALUES` +
+    ratings.map(rating => `(${movieId}, ${rating.userId}, ${rating.rating}, '${rating.time_created}')`).join(","));
 };
 
 describe("Insert Combined Data", () => {
